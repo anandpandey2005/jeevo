ffbfe5fb7dd67b978eee0b05e2e0b46af4d15ab8
ANAND PANDEY <anandpandey20005@gmail.com>
2026-04-10 23:31:15 +0530

    sidebar minimal fix

--- client/src/components/common/Sidebar.js
+++ client/src/components/common/Sidebar.js
@@ -187,17 +187,9 @@ const Sidebar = ({ isOpen = true, onClose }) => {
   };
 
   return (
-    <div className='md:block hidden'>
-      {/* Overlay for mobile */}
-      {isOpen && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          onClick={onClose}
-          className="fixed inset-0 bg-black/50 z-40 lg:hidden"
-        />
-      )}
+    <div className='lg:block hidden'>
+    
+      
 
       {/* Sidebar */}
       <motion.aside
